5f272970cb01076cafc1c644ddfd8a5af721d1a1
Newnab <adam@adamburt.com>
2014-10-07 13:06:13 +0100

    Some basic styling

--- AwkwardRoulette/AwkwardRoulette.Web.UI/scripts/main.js
+++ AwkwardRoulette/AwkwardRoulette.Web.UI/scripts/main.js
@@ -3,5 +3,14 @@ jQuery(function ($) {
     // place script here
 
 
+    $('.reveal').click(function(){
+    	var reveal = $(this).attr('href');
+    	$(this).animate({'opacity':'0'}, 500, function(){
+    		$(this).hide();
+			$(reveal).css({'opacity':'0'}).show().animate({'opacity':'1'}, 500);
+    	});
+    	
 
+    	return false;
+    });
 });     //jQuery
