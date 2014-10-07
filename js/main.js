4ee46f18e70a1f8051f4fba5fc9cdbfb401077c0
Michael Chart <michaelchart@gmail.com>
2014-10-07 11:31:33 +0100

    Login with FB

---
+++ js/main.js
@@ -0,0 +1,40 @@
+(function($) {
+	function fbEnsureInit(callback) {
+		if(!window.fbInitDone) {
+			setTimeout(function() { fbEnsureInit(callback); }, 50);
+		} else {
+			if(callback) {
+				callback();
+			}
+		}
+	}
+
+	fbEnsureInit(function() {
+		load(jQuery, FB);
+	});
+})(jQuery);
+
+
+function load($, FB) {
+
+	$(".button").on('click', function() {
+		FB.getLoginStatus(function(response) {
+			if (response.status === 'connected') {
+				console.log('Logged in.');
+				window.location.href = 'matches.html';
+			}
+			else {
+				FB.login(function() {
+					//FB.api('/me/feed', 'post', {message: 'Hello, world!'});
+					window.location.href = 'matches.html';
+
+				}, {scope: 'publish_actions'});
+
+			}
+		});
+		return false;
+	});
+
+
+
+}
