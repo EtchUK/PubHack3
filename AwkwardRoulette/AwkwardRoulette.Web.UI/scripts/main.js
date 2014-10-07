bf808cc7f8e5281fed83cca30d8437c9eb7c6f2e
Newnab <adam@adamburt.com>
2014-10-07 13:29:23 +0100

    Roulette functionality

--- AwkwardRoulette/AwkwardRoulette.Web.UI/scripts/main.js
+++ AwkwardRoulette/AwkwardRoulette.Web.UI/scripts/main.js
@@ -13,4 +13,48 @@ jQuery(function ($) {
 
     	return false;
     });
+
+    var spinning = false;
+	var countdown = 0;
+	var speed = 0;
+    $('.start-spinner').click(function(){
+    	if(spinning){
+
+    	} else {
+    		spinning = true;
+    		$('.spinner-item').removeClass('chosen').removeClass('active');
+    		speed = 0;
+    		$(this).text('Wait...');
+    		countdown = Math.floor((Math.random() * 20) + 10);
+    		nextItem();
+    	}
+
+    	return false;
+    });
+
+    function nextItem(){
+    	setTimeout(function(){
+    		if($('.spinner-item.active').length && $('.spinner-item.active').next('.spinner-item').length){
+    			$('.spinner-item.active').removeClass('active').next().addClass('active');
+    		} else {
+    			$('.spinner-item.active').removeClass('active');
+    			$('.spinner-item').first().addClass('active');
+    		}
+    		countdown = countdown - 1;
+	    	if(countdown > 0){
+	    		nextItem();
+	    	} else {
+	    		//Over - This is where we stop, so do stuff.
+	    		$('.spinner-item.active').addClass('chosen');
+	    		console.log($('.spinner-item.active').data('punishment'));
+	    		endSpin();
+	    	}
+    	}, speed);
+    	speed = speed + 50;   	
+    }
+
+    function endSpin(){
+	    spinning = false;
+		$('.start-spinner').text('Play');
+    }
 });     //jQuery
