72854a66e759076ed9f74fd0cbe4eee408ba5dd1
Michael Chart <michaelchart@gmail.com>
2014-10-07 14:20:35 +0100

    Facebook auth and posting

--- AwkwardRoulette/AwkwardRoulette.Web.UI/scripts/main.js
+++ AwkwardRoulette/AwkwardRoulette.Web.UI/scripts/main.js
@@ -1,7 +1,74 @@
-jQuery(function ($) {
+(function($) {
+	$.ajaxSetup({ cache: true });
+	$.getScript('//connect.facebook.net/en_UK/all.js', function() {
+		FB.init({
+			appId      : '729058967147863', // test app
+			//appId      : '729058753814551', // live app
+		});
 
-    // place script here
 
 
 
-});     //jQuery
+	});
+
+
+	$(".js-play").on('click', function() {
+
+		authFacebook().then(function() {
+			fbPostToWall("me", "Hi").then(function(response) {
+				console.log("posted to your timeline");
+			});
+		});
+
+		return false;
+	});
+
+	function authFacebook() {
+		var deferred = $.Deferred();
+
+		FB.getLoginStatus(function(response) {
+			if (response.status === 'connected') {
+				deferred.resolve();
+			}
+			else {
+				FB.login(function() {
+					deferred.resolve();
+				}, { scope: 'publish_actions' });
+
+			}
+		});
+
+		return deferred;
+	}
+
+	function authTwitter() {
+		
+	}
+
+	function twitterPost() {
+
+	}
+
+
+	function fbPostToWall(userId, message) {
+		var deferred = $.Deferred();
+		FB.api(
+			"/" + userId + "/feed",
+			"POST",
+			{
+				"message": message
+			},
+			function (response) {
+				if (response && !response.error) {
+					deferred.resolve(response);
+				} else if (response.error) {
+					deferred.reject(response.error);
+				}
+			}
+		);
+
+		return deferred;
+	}
+
+})(jQuery);
+
